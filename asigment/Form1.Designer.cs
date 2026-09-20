531fcaf3641ee31e6337fa3e6f512f321a813022
sihaa <sihaa@DESKTOP-E2QEOUA>
2026-09-20 16:26:24 +0300

    Add project files.

---
+++ asigment/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace asigment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dayOfWeekPromptLabel1 = new System.Windows.Forms.Label();
+            this.dayofmonthpromptlable1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.monthPromptLabel1 = new System.Windows.Forms.Label();
+            this.dayOfWeekTextBox = new System.Windows.Forms.TextBox();
+            this.yearTextBox = new System.Windows.Forms.TextBox();
+            this.dayOfMonthTextBox = new System.Windows.Forms.TextBox();
+            this.monthTextBox = new System.Windows.Forms.TextBox();
+            this.dataOutputLabel = new System.Windows.Forms.Label();
+            this.yearpromptlable1 = new System.Windows.Forms.Label();
+            this.ShowDateButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dayOfWeekPromptLabel1
+            // 
+            this.dayOfWeekPromptLabel1.AutoSize = true;
+            this.dayOfWeekPromptLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dayOfWeekPromptLabel1.Location = new System.Drawing.Point(49, 38);
+            this.dayOfWeekPromptLabel1.Name = "dayOfWeekPromptLabel1";
+            this.dayOfWeekPromptLabel1.Size = new System.Drawing.Size(282, 29);
+            this.dayOfWeekPromptLabel1.TabIndex = 0;
+            this.dayOfWeekPromptLabel1.Text = "Enter the day of the week";
+            // 
+            // dayofmonthpromptlable1
+            // 
+            this.dayofmonthpromptlable1.AutoSize = true;
+            this.dayofmonthpromptlable1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dayofmonthpromptlable1.Location = new System.Drawing.Point(49, 161);
+            this.dayofmonthpromptlable1.Name = "dayofmonthpromptlable1";
+            this.dayofmonthpromptlable1.Size = new System.Drawing.Size(382, 29);
+            this.dayofmonthpromptlable1.TabIndex = 1;
+            this.dayofmonthpromptlable1.Text = "Enter the numeric day of the month";
+            this.dayofmonthpromptlable1.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 29);
+            this.label3.TabIndex = 2;
+            // 
+            // monthPromptLabel1
+            // 
+            this.monthPromptLabel1.AutoSize = true;
+            this.monthPromptLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthPromptLabel1.Location = new System.Drawing.Point(49, 96);
+            this.monthPromptLabel1.Name = "monthPromptLabel1";
+            this.monthPromptLabel1.Size = new System.Drawing.Size(318, 29);
+            this.monthPromptLabel1.TabIndex = 3;
+            this.monthPromptLabel1.Text = "Enter the name of the  month";
+            // 
+            // dayOfWeekTextBox
+            // 
+            this.dayOfWeekTextBox.Location = new System.Drawing.Point(476, 22);
+            this.dayOfWeekTextBox.Multiline = true;
+            this.dayOfWeekTextBox.Name = "dayOfWeekTextBox";
+            this.dayOfWeekTextBox.Size = new System.Drawing.Size(270, 45);
+            this.dayOfWeekTextBox.TabIndex = 4;
+            // 
+            // yearTextBox
+            // 
+            this.yearTextBox.Location = new System.Drawing.Point(476, 248);
+            this.yearTextBox.Multiline = true;
+            this.yearTextBox.Name = "yearTextBox";
+            this.yearTextBox.Size = new System.Drawing.Size(270, 45);
+            this.yearTextBox.TabIndex = 5;
+            // 
+            // dayOfMonthTextBox
+            // 
+            this.dayOfMonthTextBox.Location = new System.Drawing.Point(476, 171);
+            this.dayOfMonthTextBox.Multiline = true;
+            this.dayOfMonthTextBox.Name = "dayOfMonthTextBox";
+            this.dayOfMonthTextBox.Size = new System.Drawing.Size(270, 45);
+            this.dayOfMonthTextBox.TabIndex = 6;
+            // 
+            // monthTextBox
+            // 
+            this.monthTextBox.Location = new System.Drawing.Point(476, 88);
+            this.monthTextBox.Multiline = true;
+            this.monthTextBox.Name = "monthTextBox";
+            this.monthTextBox.Size = new System.Drawing.Size(270, 45);
+            this.monthTextBox.TabIndex = 7;
+            // 
+            // dataOutputLabel
+            // 
+            this.dataOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dataOutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataOutputLabel.Location = new System.Drawing.Point(68, 319);
+            this.dataOutputLabel.Name = "dataOutputLabel";
+            this.dataOutputLabel.Size = new System.Drawing.Size(678, 50);
+            this.dataOutputLabel.TabIndex = 8;
+            // 
+            // yearpromptlable1
+            // 
+            this.yearpromptlable1.AutoSize = true;
+            this.yearpromptlable1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yearpromptlable1.Location = new System.Drawing.Point(49, 238);
+            this.yearpromptlable1.Name = "yearpromptlable1";
+            this.yearpromptlable1.Size = new System.Drawing.Size(161, 29);
+            this.yearpromptlable1.TabIndex = 9;
+            this.yearpromptlable1.Text = "Enter the year";
+            // 
+            // ShowDateButton
+            // 
+            this.ShowDateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShowDateButton.Location = new System.Drawing.Point(122, 398);
+            this.ShowDateButton.Name = "ShowDateButton";
+            this.ShowDateButton.Size = new System.Drawing.Size(160, 40);
+            this.ShowDateButton.TabIndex = 10;
+            this.ShowDateButton.Text = "Show Date";
+            this.ShowDateButton.UseVisualStyleBackColor = true;
+            this.ShowDateButton.Click += new System.EventHandler(this.ShowDateButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearButton.Location = new System.Drawing.Point(508, 398);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(160, 40);
+            this.ClearButton.TabIndex = 11;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 512);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.ShowDateButton);
+            this.Controls.Add(this.yearpromptlable1);
+            this.Controls.Add(this.dataOutputLabel);
+            this.Controls.Add(this.monthTextBox);
+            this.Controls.Add(this.dayOfMonthTextBox);
+            this.Controls.Add(this.yearTextBox);
+            this.Controls.Add(this.dayOfWeekTextBox);
+            this.Controls.Add(this.monthPromptLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dayofmonthpromptlable1);
+            this.Controls.Add(this.dayOfWeekPromptLabel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label dayOfWeekPromptLabel1;
+        private System.Windows.Forms.Label dayofmonthpromptlable1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label monthPromptLabel1;
+        private System.Windows.Forms.TextBox dayOfWeekTextBox;
+        private System.Windows.Forms.TextBox yearTextBox;
+        private System.Windows.Forms.TextBox dayOfMonthTextBox;
+        private System.Windows.Forms.TextBox monthTextBox;
+        private System.Windows.Forms.Label dataOutputLabel;
+        private System.Windows.Forms.Label yearpromptlable1;
+        private System.Windows.Forms.Button ShowDateButton;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ asigment/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asigment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowDateButton_Click(object sender, EventArgs e)
+        {
+            //creating variables
+            string dayOfWeek, nameOfMonth;
+            string numericDayOfMonth, year;
+
+            string fullInfo;
+
+            dayOfWeek = dayOfWeekTextBox.Text;
+            nameOfMonth = monthTextBox.Text;
+            numericDayOfMonth = dayOfMonthTextBox.Text;
+            year = yearTextBox.Text;
+
+            fullInfo = dayOfWeek + ", " + nameOfMonth + " " + numericDayOfMonth + ", " + year;
+
+            dataOutputLabel.Text = fullInfo;
+
+
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            dataOutputLabel.Text = " ";
+
+        }
+    }
+}
